aad87fee4952870e2a0ba8e9779eba58504c62ba
bbottema <b.bottema@projectnibble.org>
2024-03-03 22:44:09 +0100

    Made equipment repo's generic for all equipment types.

---
+++ src/app/orm/repositories/base_equipment_repository.py
@@ -0,0 +1,40 @@
+from typing import TypeVar, Type
+
+from sqlalchemy.orm import Session
+
+from app.config.autowire import component
+from app.orm.entities import ObservationSite
+from app.orm.repositories.base_repository import BaseRepository, NamedEntity
+
+
+class EquipmentEntity(NamedEntity):
+    observation_sites: list[ObservationSite]
+
+
+T = TypeVar('T', bound=EquipmentEntity)
+
+
+@component
+class BaseEquipmentRepository(BaseRepository[T]):
+    def __init__(self, entity: Type[T]):
+        super().__init__(entity)
+
+    def handle_update(self, persisted_telescope: T, updated_telescope: T, session: Session):
+
+        # Convert lists to sets for efficient look-up
+        persisted_site_ids = {site.id for site in persisted_telescope.observation_sites}
+        updated_site_ids = {site.id for site in updated_telescope.observation_sites}
+
+        # Determine sites to add and remove
+        sites_to_add_ids = updated_site_ids - persisted_site_ids
+        sites_to_remove_ids = persisted_site_ids - updated_site_ids
+
+        # Remove sites
+        for site in persisted_telescope.observation_sites[:]:
+            if site.id in sites_to_remove_ids:
+                persisted_telescope.observation_sites.remove(site)
+
+        # Add new sites
+        for site_id in sites_to_add_ids:
+            new_site = session.query(ObservationSite).filter(ObservationSite.id == site_id).one()
+            persisted_telescope.observation_sites.append(new_site)
--- src/app/orm/repositories/telescope_repository.py
+++ src/app/orm/repositories/telescope_repository.py
@@ -1,33 +1,9 @@
-from typing import Type
-
-from sqlalchemy.orm import Session
-
 from app.config.autowire import component
-from app.orm.entities import Telescope, ObservationSite
-from app.orm.repositories.base_repository import BaseRepository
+from app.orm.entities import Telescope
+from app.orm.repositories.base_equipment_repository import BaseEquipmentRepository
 
 
 @component
-class TelescopeRepository(BaseRepository[Telescope]):
+class TelescopeRepository(BaseEquipmentRepository[Telescope]):
     def __init__(self):
         super().__init__(Telescope)
-
-    def handle_update(self, persisted_telescope: Telescope, updated_telescope: Telescope, session: Session):
-
-        # Convert lists to sets for efficient look-up
-        persisted_site_ids = {site.id for site in persisted_telescope.observation_sites}
-        updated_site_ids = {site.id for site in updated_telescope.observation_sites}
-
-        # Determine sites to add and remove
-        sites_to_add_ids = updated_site_ids - persisted_site_ids
-        sites_to_remove_ids = persisted_site_ids - updated_site_ids
-
-        # Remove sites
-        for site in persisted_telescope.observation_sites[:]:
-            if site.id in sites_to_remove_ids:
-                persisted_telescope.observation_sites.remove(site)
-
-        # Add new sites
-        for site_id in sites_to_add_ids:
-            new_site = session.query(ObservationSite).filter(ObservationSite.id == site_id).one()
-            persisted_telescope.observation_sites.append(new_site)
