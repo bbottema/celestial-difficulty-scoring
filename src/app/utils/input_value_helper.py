c34abcfbec58ba8ed5118ebd1023bc49e8f467dc
bbottema <b.bottema@projectnibble.org>
2024-03-16 11:06:17 +0100

    Fixed overload definition, getting rid of some warnings

--- src/app/utils/input_value_helper.py
+++ src/app/utils/input_value_helper.py
@@ -5,12 +5,12 @@ def parse_str_float(value: str | None) -> float | None:
     return float(value.replace(',', '.')) if value else None
 
 
-# @overload
-# def parse_str_int(value: None) -> None: ...
-#
-#
-# @overload
-# def parse_str_int(value: str) -> int: ...
+@overload
+def parse_str_int(value: Literal[None]) -> None: ...
+
+
+@overload
+def parse_str_int(value: str) -> int: ...
 
 
 def parse_str_int(value: str | None) -> int | None:
